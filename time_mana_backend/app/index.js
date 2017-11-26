28cdc833e7a3669e2205ea214967f9a0a2f05f7d
tukida <tuankiet.hcmc@gmail.com>
2017-11-26 09:57:28 +0700

    allow authorization header

--- time_mana_backend/app/index.js
+++ time_mana_backend/app/index.js
@@ -38,7 +38,7 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header(
     'Access-Control-Allow-Headers',
-    'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
+    'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Authorization'
   );
   next();
 });
