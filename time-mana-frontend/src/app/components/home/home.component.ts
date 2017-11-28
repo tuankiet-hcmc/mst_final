2e6f99cb1bf14fb1deaf8f7b6b388c58a3983d53
tukida <tuankiet.hcmc@gmail.com>
2017-11-29 00:45:55 +0700

    fix router

--- time-mana-frontend/src/app/components/home/home.component.ts
+++ time-mana-frontend/src/app/components/home/home.component.ts
@@ -1,23 +1,29 @@
 import { Component, OnInit, style } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor(private router: Router) { }
+  constructor(private userService: UserService, private router: Router) {}
 
   ngOnInit() {
+    if (this.userService.loggedIn()) {
+      this.router.navigate(['/listevent']);
+      return true; // Return true: User is allowed to view route
+    } else {
+      this.router.navigate(['/']); // Return error and route to login page
+      return false; // Return false: user not authorized to view page
+    }
   }
 
-  signup () {
+  signup() {
     this.router.navigate(['/register']);
-  };
-  signin () {
+  }
+  signin() {
     this.router.navigate(['/login']);
-  };
-
+  }
 }
--- time-mana-frontend/src/app/guards/auth.guard.ts
+++ time-mana-frontend/src/app/guards/auth.guard.ts
@@ -22,7 +22,7 @@ export class AuthGuard implements CanActivate {
       return true; // Return true: User is allowed to view route
     } else {
       this.redirectUrl = state.url; // Grab previous urul
-      this.router.navigate(['/login']); // Return error and route to login page
+      this.router.navigate(['/']); // Return error and route to login page
       return false; // Return false: user not authorized to view page
     }
   }
